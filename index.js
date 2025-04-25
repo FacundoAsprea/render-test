37efe8f28ac014e237879c3ba95502c495bfb3db
FacundoAsprea <facundoasprea09@gmail.com>
2025-04-25 17:22:23 -0300

    first back commmit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const cors = require('cors')
+app = express() 
+app.use(cors())
+app.use(express.json())
+
+let persons = [
+        {
+            id: "1",
+            name: "pepe",
+            number: "777"
+        },
+        {
+            id: "2",
+            name: "papugomez",
+            number: "9999"
+        },
+        {
+            id: "3",
+            name: "mamaaaaa uuuuuuu",
+            number: "8181"
+        }
+  ]
+
+app.get('/persons', (req, res) => {
+    res.send(persons)
+})
+
+app.get('/persons/:id', (req, res) => {
+    let id = req.params.id
+    let chosenPerson = persons.find(person => person.id === id)
+    chosenPerson ? res.send(chosenPerson) : res.status(404).end()
+})
+
+app.post('/persons', (req, res) => {
+    persons.push(req.body)
+    res.end()
+})
+
+app.delete('/persons/:id', (req, res) => {
+    let id = req.params.id
+    let chosenPerson = persons.find(person => person.id === id)
+    if(chosenPerson) {
+        persons = persons.filter(person => person != chosenPerson)
+        res.end()
+    }
+    else {
+        res.status(404).end()
+    }
+})
+
+
+app.listen(3001, () => console.log('PORT 3001'))
