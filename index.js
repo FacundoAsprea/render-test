582cd5260f1d92d474f2a6e1cb43fe8349864362
FacundoAsprea <facundoasprea09@gmail.com>
2025-05-01 17:26:13 -0300

    puede ser pa?

--- index.js
+++ index.js
@@ -1,62 +1,76 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+
+const mongoDB = require('./models/Person.js')
+
 app = express() 
 app.use(cors())
 app.use(express.json())
 
 app.use(express.static('dist'))
 
+let persons
+const updateUserList = () => {
+    return new Promise((resolve, reject) => {
+        mongoDB.getStoredUsers()
+        .then((result) => {
+            persons = result
+            resolve(true)
+        })
+    })
+}
+
 const PORT = process.env.PORT || 3001
 
-let persons = [
-        {
-            id: "1",
-            name: "kkkkkkkkkkkk",
-            number: "777"
-        },
-        {
-            id: "2",
-            name: "papugomez",
-            number: "9999"
-        },
-        {
-            id: "3",
-            name: "zzzzzzzzzzz",
-            number: "8181"
-        }
-  ]
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    next(error)
+  }
 
 app.get('/', (req, res) => {
     res.end()
 })
 
 app.get('/persons', (req, res) => {
-    console.log("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-    res.send(persons)
+    updateUserList()
+    .then(() => res.send(persons))
 })
 
 app.get('/persons/:id', (req, res) => {
-    let id = req.params.id
-    let chosenPerson = persons.find(person => person.id === id)
-    chosenPerson ? res.send(chosenPerson) : res.status(404).end()
+    updateUserList()
+    .then(() => {
+        let id = req.params.id
+        let chosenPerson = persons.find(person => person.id === id)
+        chosenPerson ? res.send(chosenPerson) : res.status(404).end()
+    })
 })
 
 app.post('/persons', (req, res) => {
-    persons.push(req.body)
+    mongoDB.addUser(req.body)
     res.end()
 })
 
-app.delete('/persons/:id', (req, res) => {
-    let id = req.params.id
-    let chosenPerson = persons.find(person => person.id === id)
-    if(chosenPerson) {
-        persons = persons.filter(person => person != chosenPerson)
-        res.end()
-    }
-    else {
-        res.status(404).end()
-    }
+app.delete('/persons/:id', (req, res, next) => {
+    updateUserList()
+    .then(() => {
+        let id = req.params.id
+        mongoDB.eraseUser(id)
+        .then(() => res.status(204).end())
+        .catch(err => next(err))
+    })
 })
 
+app.put('/persons/:id', (req, res, next) => {
+    let id = req.params.id
+    mongoDB.replaceUser(id, req.body)
+    .then(() => res.end())
+    .catch((err) => next(err))
+})
 
+app.use(errorHandler)
 app.listen(PORT, () => console.log(`Puerto: ${PORT}`))
---
+++ models/Person.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+
+mongoURL = dotenv.parsed.MONGO_URL
+
+mongoose.connect(mongoURL)
+.then(() => console.log("Connected to MongoDB", mongoURL))
+.catch((err) => console.error("An error ocurred while trying to connect to MongoDB: ", err))
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    phoneNumber: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+const getStoredUsers = () => {
+    return (
+        Person.find({})
+        .then(result => {
+            return Object.values(result)
+        })
+    )
+}
+
+const addUser = ( { name, phoneNumber } ) => {
+    const newUser = new Person({
+        name: name,
+        phoneNumber: phoneNumber
+    })
+    newUser.save()
+    .then(newUser => {
+        console.log("Added a new user: ", newUser)
+    })
+}
+
+const eraseUser = (userId) => Person.findByIdAndDelete(userId)
+
+const replaceUser = (userId, updatedUser) => Person.findByIdAndUpdate(userId, updatedUser, { new: true })
+
+module.exports = { getStoredUsers, addUser, eraseUser, replaceUser }
